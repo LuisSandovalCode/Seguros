407e0863c8bb7a4465d69ea10defaa575c7b3546
LUIS SANDOVAL <luissandoval1897@hotmail.com>
2020-07-04 20:30:06 -0600

    Customer structure API

---
+++ Seguros/Seguros.Api/Controllers/CustomerController.cs
@@ -0,0 +1,95 @@
+﻿using Seguros.Business;
+using Seguros.Business.Utils;
+using Seguros.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Seguros.Api.Controllers
+{
+    [RoutePrefix("api/v1/Customer")]
+    public class CustomerController : ApiController
+    {
+        BusinessCustomer BusinessCustomer;
+        public CustomerController()
+        {
+            BusinessCustomer = new BusinessCustomer();
+        }
+
+        [Route("Create")]
+        [HttpPost]
+        public IHttpActionResult Create()
+        {
+            try
+            {
+                var RequestBody = Request.Content.ReadAsStringAsync().Result;
+
+                var customer = RequestBody.DeserealizeJson<Customer>();
+
+                return Ok(BusinessCustomer.Create(customer));
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError();
+            }
+        }
+
+        [Route("Update")]
+        [HttpPost]
+        public IHttpActionResult Update()
+        {
+            try
+            {
+                var RequestBody = Request.Content.ReadAsStringAsync().Result;
+
+                var customer = RequestBody.DeserealizeJson<Customer>();
+
+                return Ok(BusinessCustomer.Update(customer));
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError();
+            }
+        }
+
+        [Route("Delete")]
+        [HttpPost]
+        public IHttpActionResult Delete()
+        {
+            try
+            {
+                var RequestBody = Request.Content.ReadAsStringAsync().Result;
+
+                var customer = RequestBody.DeserealizeJson<Customer>();
+
+                return Ok(BusinessCustomer.Delete(customer));
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError();
+            }
+        }
+
+        [Route("CustomerList")]
+        [HttpPost]
+        public IHttpActionResult GetCustomers()
+        {
+            try
+            {
+                var RequestBody = Request.Content.ReadAsStringAsync().Result;
+
+                var customer = RequestBody.DeserealizeJson<Customer>();
+
+                return Ok(BusinessCustomer.GetEntities(customer));
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError();
+            }
+        }
+
+    }
+}
---
+++ Seguros/Seguros.Business/BusinessCustomer.cs
@@ -0,0 +1,75 @@
+﻿using Seguros.Business.Utils;
+using Seguros.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seguros.Business
+{
+    public class BusinessCustomer : IBusiness
+    {
+        Repository repositoryCustomer;
+        public BusinessCustomer()
+        {
+            repositoryCustomer = new Repository();
+        }
+
+        public bool Create<Entity>(Entity entity) where Entity : class
+        {
+            try
+            {
+                var prm = entity.GetParameters();
+
+                return repositoryCustomer.InsertEntity("SP_CreateCustomer", prm);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool Delete<Entity>(Entity entity) where Entity : class
+        {
+            try
+            {
+                var prm = entity.GetParameters();
+
+                return repositoryCustomer.InsertEntity("SP_DeleteCustomer", prm);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public List<Entity> GetEntities<Entity>(Entity entity)
+        {
+            try
+            {
+                var prm = entity.GetParameters();
+
+                return repositoryCustomer.GetEntities<Entity>("SP_GetCustomer",prm);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool Update<Entity>(Entity entity) where Entity : class
+        {
+            try
+            {
+                var prm = entity.GetParameters();
+
+                return repositoryCustomer.InsertEntity("SP_UpdateCustomer", prm);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
